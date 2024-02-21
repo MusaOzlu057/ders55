8177d9c8866b3ddd56cadf029327b7a4cf8a5d6c
MusaOzlu057 <159758324+MusaOzlu057@users.noreply.github.com>
2024-02-21 14:07:30 +0300

    Add files via upload

---
+++ control.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+ <?php
+ $adisoyadi=$_POST['adisoyadi'];
+ $mesaj= $_POST['mesaj'];
+ echo "<div class='guvensiz'>
+       <h4> Güvensiz Form</h4>
+        <table>
+            <tr>
+                <td>Gönderen:</td>
+                <td>$adisoyadi</td>
+            </tr>
+            <tr>
+                <td>Mesaj:</td>
+                <td>$mesaj</td>
+            </tr>
+        </table>
+        </div>";
+         $gadisoyadi=htmlengelle($adisoyadi);
+ $gmesaj= guvenli_icerik($mesaj);
+ function htmlengelle($gelen){
+    $giden = htmlspecialchars($gelen);
+    $giden = htmlentities($giden); 
+    return $giden;
+ }
+ function guvenli_icerik($gelen){
+    $giden = strip_tags($gelen);
+    $giden = addslashes($giden);
+    return $giden;
+ }
+echo "<div class='guvenli'>
+ <h4>Güvenli Form</h4>
+ <table>
+ <tr>
+ <td>Gönderen:</td>
+ <td>$gadisoyadi</td>
+ </tr>
+ <tr>
+ <td>Mesaj:</td>
+ <td>$gmesaj</td>
+ </tr>
+ </table>
+ </div>"; 
+?>
+</body>
+</html>
